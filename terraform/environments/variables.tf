24217e8ce973180b338eb2841aaa45aa0428f293
LakshanyaP <lakshanya0501@gmail.com>
2026-09-19 21:28:49 +0800

    test terraform destroy

--- terraform/environments/variables.tf
+++ terraform/environments/variables.tf
@@ -28,7 +28,7 @@ variable "shared_rg_name" {
 
 variable "shared_acr_name" {
   type        = string
-  default     = "acrewasteplatform"
+  default     = "acrewasteplatform_lak"
   description = "Name of the shared Azure Container Registry."
 }
 
